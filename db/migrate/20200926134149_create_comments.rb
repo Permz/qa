d55caf84c9cf8b98188d8d1eebe1e9947a3fdb4b
Permz <sakanaquarium0mashim0@gmail.com>
2020-09-26 23:57:39 +0900

    comment model created

--- db/migrate/20200926134149_create_comments.rb
+++ db/migrate/20200926134149_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :content
-      t.references :qustion, null: false, foreign_key: true
+      t.references :qustion, null: false, foreign_key: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_133649) do
+ActiveRecord::Schema.define(version: 2020_09_26_134149) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content"
+    t.bigint "qustion_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["qustion_id"], name: "index_comments_on_qustion_id"
+  end
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
